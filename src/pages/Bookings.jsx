a4fd0aea243e287d45ef768d1e607ad980338365
Tihomir Zhelyazkov <tihomirtx88@gmail.com>
2024-03-02 15:48:47 +0200

    Create basic pages

---
+++ src/pages/Bookings.jsx
@@ -0,0 +1,9 @@
+import Heading from "../ui/Heading";
+import Row from "../ui/Row";
+
+export default function Bookings() {
+  <Row type="horizontal">
+    <Heading as="h1">All bookings</Heading>
+    <p>TEST</p>
+  </Row>;
+}
