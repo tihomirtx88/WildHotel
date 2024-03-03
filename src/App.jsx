e46ad6517051df9fc2495b2a7f66253261742f41
Tihomir Zhelyazkov <tihomirtx88@gmail.com>
2024-03-03 18:20:24 +0200

    Create settings components and add react-router routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
-import { BrowserRouter, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import GloablStyles from "./styles/GlobalStyles";
 
-
+import Dashboard from './pages/Dashboard';
+import Bookings from './pages/Bookings';
+import Cabins from './pages/Cabins';
+import Users from './pages/Users';
+import Settings from './pages/Settings';
+import Account from './pages/Account';
+import Login from './pages/Login';
+import PageNotFound from './pages/PageNotFound';
 
 function App() {
   return (
@@ -9,8 +16,15 @@ function App() {
       <GloablStyles>
        <BrowserRouter>
          <Routes>
-
-
+           <Route index path="dashboard" element={<Navigate replace to="dashboard"/>}/>
+           <Route path="dashboard" element={<Dashboard/>}/>
+           <Route path="bookings" element={<Bookings/>}/>
+           <Route path="cabins" element={<Cabins/>}/>
+           <Route path="users" element={<Users/>}/>
+           <Route path="settings" element={<Settings/>}/>
+           <Route path="account" element={<Account/>}/>
+           <Route path="login" element={<Login/>}/>
+           <Route path="*" element={<PageNotFound/>}/>
          </Routes>
        </BrowserRouter>
       </GloablStyles>
