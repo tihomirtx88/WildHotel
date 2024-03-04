1ccfe5427690fca4242d8b9fd3236a0fd266e01e
Tihomir Zhelyazkov <tihomirtx88@gmail.com>
2024-03-04 17:50:46 +0200

    Fetch cabin data and deleted cabin functionality

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -1,6 +1,13 @@
 import styled from "styled-components";
+import {useDeleteCabin} from './useDeleteCabin';
+import {formatCurrency}  from './../../utils/helpers';
 
-export const TableRow = styled.div`
+import { 
+  // HiPencil, 
+  // HiSquare2Stack, 
+  HiTrash } from "react-icons/hi2";
+
+ const TableRow = styled.div`
   display: grid;
   grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
   column-gap: 2.4rem;
@@ -12,7 +19,7 @@ export const TableRow = styled.div`
   }
 `;
 
-export const Img = styled.img`
+ const Img = styled.img`
   display: block;
   width: 6.4rem;
   aspect-ratio: 3 / 2;
@@ -21,20 +28,37 @@ export const Img = styled.img`
   transform: scale(1.5) translateX(-7px);
 `;
 
-export const Cabin = styled.div`
+ const Cabin = styled.div`
   font-size: 1.6rem;
   font-weight: 600;
   color: var(--color-grey-600);
   font-family: "Sono";
 `;
 
-export const Price = styled.div`
+ const Price = styled.div`
   font-family: "Sono";
   font-weight: 600;
 `;
 
-export const Discount = styled.div`
+ const Discount = styled.div`
   font-family: "Sono";
   font-weight: 500;
   color: var(--color-green-700);
 `;
+
+
+export default function CabinRow({cabin}){
+  const { isDeleting, deleteCabin } = useDeleteCabin();
+
+  const {name, maxCapacity, regularPrice, discount, image,  id: cabinId,} = cabin;
+  return(
+    <TableRow role="row">
+      <Img src={image}/>
+      <Cabin>{name}</Cabin>
+      <div>Fits up to {maxCapacity} guests</div>
+      <Price>{formatCurrency(regularPrice)}</Price>
+      <Discount>{formatCurrency(discount)}</Discount>
+      <button onClick={()=> deleteCabin(cabinId)} disabled={isDeleting}><HiTrash /></button>
+    </TableRow>
+  );
+}
--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,14 +1,23 @@
 import supabase from "./supabase.";
 
-export  async function getCabins(){
-    const { data, error } = await supabase
-    .from('cabins')
-    .select('*')
+export async function getCabins() {
+  const { data, error } = await supabase.from("cabins").select("*");
 
-    if(error){
-        console.error(error.message);
-        throw new Error("Cabins could not be loaded");
-    }
+  if (error) {
+    console.error(error.message);
+    throw new Error("Cabins could not be loaded");
+  }
 
-    return data;
+  return data;
+}
+
+export async function deleteCabin(id) {
+  const {data, error } = await supabase.from("cabins").delete().eq("id", id);
+
+  if (error) {
+    console.error(error.message);
+    throw new Error("Cabins could not be deleted");
+  }
+
+  return data;
 }
