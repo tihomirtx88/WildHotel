631437304bc2313a765f54966e05891e36548bbc
Tihomir Zhelyazkov <tihomirtx88@gmail.com>
2024-03-04 19:05:29 +0200

    Add toast library and react-form library

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import Account from "./pages/Account";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./ui/AppLayout";
+import { Toaster } from "react-hot-toast";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -40,6 +41,26 @@ function App() {
           <Route path="login" element={<Login />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
+        <Toaster
+          position="top-right"
+          gutter={12}
+          containerStyle={{ margin: "8px" }}
+          toastOptions={{
+            success: {
+              duration: 3000,
+            },
+            error: {
+              duration: 5000,
+            },
+            style: {
+              fontSize: "16px",
+              maxWidth: "500px",
+              padding: "16px 24px",
+              backgroundColor: "var(--color-gray-0)",
+              color: "var(--color-gray-700)",
+            },
+          }}
+        />
       </BrowserRouter>
     </QueryClientProvider>
   );
--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -1,13 +1,14 @@
 import styled from "styled-components";
-import {useDeleteCabin} from './useDeleteCabin';
-import {formatCurrency}  from './../../utils/helpers';
+import { useDeleteCabin } from "./useDeleteCabin";
+import { formatCurrency } from "./../../utils/helpers";
 
-import { 
-  // HiPencil, 
-  // HiSquare2Stack, 
-  HiTrash } from "react-icons/hi2";
+import {
+  // HiPencil,
+  // HiSquare2Stack,
+  HiTrash,
+} from "react-icons/hi2";
 
- const TableRow = styled.div`
+const TableRow = styled.div`
   display: grid;
   grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
   column-gap: 2.4rem;
@@ -19,7 +20,7 @@ import {
   }
 `;
 
- const Img = styled.img`
+const Img = styled.img`
   display: block;
   width: 6.4rem;
   aspect-ratio: 3 / 2;
@@ -28,37 +29,50 @@ import {
   transform: scale(1.5) translateX(-7px);
 `;
 
- const Cabin = styled.div`
+const Cabin = styled.div`
   font-size: 1.6rem;
   font-weight: 600;
   color: var(--color-grey-600);
   font-family: "Sono";
 `;
 
- const Price = styled.div`
+const Price = styled.div`
   font-family: "Sono";
   font-weight: 600;
 `;
 
- const Discount = styled.div`
+const Discount = styled.div`
   font-family: "Sono";
   font-weight: 500;
   color: var(--color-green-700);
 `;
 
+const ButtonDelete = styled.button`
+  padding: 5px;
+  max-width: 100px;
+`;
 
-export default function CabinRow({cabin}){
+export default function CabinRow({ cabin }) {
   const { isDeleting, deleteCabin } = useDeleteCabin();
 
-  const {name, maxCapacity, regularPrice, discount, image,  id: cabinId,} = cabin;
-  return(
+  const {
+    name,
+    maxCapacity,
+    regularPrice,
+    discount,
+    image,
+    id: cabinId,
+  } = cabin;
+  return (
     <TableRow role="row">
-      <Img src={image}/>
+      <Img src={image} />
       <Cabin>{name}</Cabin>
       <div>Fits up to {maxCapacity} guests</div>
       <Price>{formatCurrency(regularPrice)}</Price>
       <Discount>{formatCurrency(discount)}</Discount>
-      <button onClick={()=> deleteCabin(cabinId)} disabled={isDeleting}><HiTrash /></button>
+      <ButtonDelete onClick={() => deleteCabin(cabinId)} disabled={isDeleting}>
+        <HiTrash />
+      </ButtonDelete>
     </TableRow>
   );
 }
