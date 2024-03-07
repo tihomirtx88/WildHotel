8cebf9155473cf7ef6b8271a2497f7690504f2ec
Tihomir Zhelyazkov <tihomirtx88@gmail.com>
2024-03-07 17:49:42 +0200

    Create functionality for upload files

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,4 +1,4 @@
-import supabase from "./supabase.";
+import supabase, { supabaseUrl } from "./supabase.";
 
 export async function getCabins() {
   const { data, error } = await supabase.from("cabins").select("*");
@@ -12,7 +12,7 @@ export async function getCabins() {
 }
 
 export async function deleteCabin(id) {
-  const {data, error } = await supabase.from("cabins").delete().eq("id", id);
+  const { data, error } = await supabase.from("cabins").delete().eq("id", id);
 
   if (error) {
     console.error(error.message);
@@ -22,19 +22,39 @@ export async function deleteCabin(id) {
   return data;
 }
 
-export async function createEditCabin(newCabin){
+export async function createEditCabin(newCabin) {
+  const hasImagePath = newCabin.image?.startsWith?.(supabaseUrl);
+  // Crate unique name and replace // with one server will create
+  const imageName = `${Math.random()}-${newCabin.image.name}`.replaceAll(
+    "/",
+    ""
+  );
+  const imagePath = hasImagePath ? newCabin.image : `${supabaseUrl}/storage/v1/object/public/cabins-images/${imageName}`;
+  // 1 Create cabin
 
   const { data, error } = await supabase
-  .from('cabins')
-  .insert([
-    newCabin
-  ])
-  .select()
+    .from("cabins")
+    .insert([{ ...newCabin, image: imagePath }])
+    .select();
 
   if (error) {
     console.error(error.message);
     throw new Error("Cabins could not be created");
   }
+  //2 Upload image
+  if (hasImagePath) return data;
 
+  const { error: storageError } = await supabase.storage
+    .from("cabins-images")
+    .upload(imageName, newCabin.image);
+
+  //3. Delete the cabin IF there was an error uplaoding image
+  if (storageError) {
+    await supabase.from("cabins").delete().eq("id", data.id);
+    console.error(storageError);
+    throw new Error(
+      "Cabin image could not be uploaded and the cabin was not created"
+    );
+  }
   return data;
 }
--- src/services/supabase..js
+++ src/services/supabase..js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
-const supabaseUrl = 'https://mpwacdlynnypykpzgjtg.supabase.co';
+export const supabaseUrl = 'https://mpwacdlynnypykpzgjtg.supabase.co';
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1wd2FjZGx5bm55cHlrcHpnanRnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDI5OTg5MDYsImV4cCI6MjAxODU3NDkwNn0.G_PqgT3QLP8sCJ4GumTFcNicvYKq99sSBY6xcRcfF4Y";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
