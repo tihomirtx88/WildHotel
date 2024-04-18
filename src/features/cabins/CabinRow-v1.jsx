307f79305663651a8f580471be94a48c16475a67
Tihomir Zhelyazkov <tihomirtx88@gmail.com>
2024-04-18 17:10:44 +0300

    Building reusable table

--- src/features/cabins/CabinRow-v1.jsx
+++ src/features/cabins/CabinRow-v1.jsx
@@ -7,7 +7,18 @@ import { HiPencil, HiSquare2Stack, HiTrash } from "react-icons/hi2";
 import { useCrateCabin } from "./useCreateCabins";
 import Modal from "../../ui/Modal";
 import ConfirmDelete from "../../ui/ConfirmDelete";
-import Table from "../../ui/Table";
+
+const TableRow = styled.div`
+  display: grid;
+  grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
+  column-gap: 2.4rem;
+  align-items: center;
+  padding: 1.4rem 2.4rem;
+
+  &:not(:last-child) {
+    border-bottom: 1px solid var(--color-grey-100);
+  }
+`;
 
 const Img = styled.img`
   display: block;
@@ -67,7 +78,7 @@ export default function CabinRow({ cabin }) {
   }
 
   return (
-    <Table.Row role="row">
+    <TableRow role="row">
       <Img src={image} />
       <Cabin>{name}</Cabin>
       <div>Fits up to {maxCapacity} guests</div>
@@ -108,6 +119,6 @@ export default function CabinRow({ cabin }) {
           </Modal.Window>
         </Modal>
       </div>
-    </Table.Row>
+    </TableRow>
   );
 }
