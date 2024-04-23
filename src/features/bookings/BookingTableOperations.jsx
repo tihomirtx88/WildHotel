50afa49f6039dd59f32c4798190fd407ff8f4aa1
Tihomir Zhelyazkov <tihomirtx88@gmail.com>
2024-04-23 21:39:09 +0300

    Create filter functionality

--- src/features/bookings/BookingTableOperations.jsx
+++ src/features/bookings/BookingTableOperations.jsx
@@ -6,8 +6,8 @@ function BookingTableOperations() {
   return (
     <TableOperations>
       <Filter
-        filterField="status"
-        options={[
+        filterfield="status"
+        optionsArray={[
           { value: "all", label: "All" },
           { value: "checked-out", label: "Checked out" },
           { value: "checked-in", label: "Checked in" },
--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,8 +1,12 @@
 import { getToday } from "../utils/helpers";
 import supabase from "./supabase.";
 
-export async function getBookings(){
-  const { data, error} = await supabase.from("bookings").select("id, created_at, startDate, endDate, numNight, numGuests, status, totalPrice, cabins(name), guests(fullName, email)");
+export async function getBookings({filter}){
+  let query = supabase.from("bookings").select("id, created_at, startDate, endDate, numNight, numGuests, status, totalPrice, cabins(name), guests(fullName, email)");
+  
+  // FILTER
+  if(filter) query = query[filter.method || "eq"](filter.field, filter.value);
+  const { data, error} = await query;
 
   if (error) {
     console.error(error.message);
