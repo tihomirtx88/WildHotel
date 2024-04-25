0e2700055bc04d1c8e8c5a00b306c1501e172fbe
Tihomir Zhelyazkov <tihomirtx88@gmail.com>
2024-04-25 19:18:04 +0300

    Create sort bookings

--- src/ui/SortBy.jsx
+++ src/ui/SortBy.jsx
@@ -3,7 +3,7 @@ import Select from "./Select";
 
 export default function SortBy({options}){
     const [searchParams, setSearchParams] = useSearchParams();
-    const sortByValue = searchParams.get("sortBy") || "";
+    const sortBy = searchParams.get("sortBy") || "";
 
     function handleChange(e){
        searchParams.set('sortBy', e.target.value);
@@ -12,6 +12,6 @@ export default function SortBy({options}){
     }
 
     return(
-        <Select options={options} type='white' value={sortByValue} onChange={handleChange}/>
+        <Select options={options} type='white' value={sortBy} onChange={handleChange}/>
     );
 }
