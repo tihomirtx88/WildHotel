e2cfcb6b46864f816f8b6592b6f0c961fda3eef6
Tihomir Zhelyazkov <tihomirtx88@gmail.com>
2024-05-06 13:54:38 +0300

    Implementing sign up form and logic

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,5 +1,11 @@
-import Heading from './../ui/Heading';
+import Heading from "./../ui/Heading";
+import SignupForm from "../features/authentication/SignupForm";
 
-export default function NewUsers(){
-    return <Heading as="h1">Create a new user</Heading>;
+export default function Users() {
+  return (
+    <>
+      <Heading as="h1">Create a new user</Heading>
+      <SignupForm />
+    </>
+  );
 }
