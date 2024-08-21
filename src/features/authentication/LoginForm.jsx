a41910066b71fb7aef1b3c0c604f7e9adec49e3c
Tihomir Zhelyazkov <tihomirtx88@gmail.com>
2024-08-21 21:07:11 +0300

    Fix style bug in login component

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -1,11 +1,26 @@
-
 import FormRowVertical from "../../ui/FormRowVertical";
 import Input from "../../ui/Input";
-import Button from '../../ui/Button';
-import SpinnerMini from '../../ui/SpinnerMini';
+import Button from "../../ui/Button";
+import SpinnerMini from "../../ui/SpinnerMini";
 import { useState } from "react";
 import { useLogin } from "./useLogin";
 import { useNavigate } from "react-router";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const StyledLink = styled(Link)`
+  /* Add your styles here */
+  color: #007bff;
+  text-decoration: none;
+  font-weight: bold;
+  margin-top: 10px;
+  display: inline-block;
+
+  &:hover {
+    color: #0056b3;
+    text-decoration: underline;
+  }
+`;
 
 export default function LoginForm() {
   const [email, setEmail] = useState("superAdmin@abv.bg");
@@ -16,14 +31,17 @@ export default function LoginForm() {
   const { isLoadingLogin, loginData } = useLogin();
 
   function handleSubmit(e) {
-     e.preventDefault();
-     if(!email || !password) return;
-     loginData({email, password}, {
-      onSettled: () => {
-        setEmail("");
-        setPassword("");
+    e.preventDefault();
+    if (!email || !password) return;
+    loginData(
+      { email, password },
+      {
+        onSettled: () => {
+          setEmail("");
+          setPassword("");
+        },
       }
-     });
+    );
   }
 
   return (
@@ -51,11 +69,14 @@ export default function LoginForm() {
       </FormRowVertical>
       <FormRowVertical>
         <Button size="large" disabled={isLoadingLogin}>
-           {!isLoadingLogin ? 'Log in' : <SpinnerMini/>}
+          {!isLoadingLogin ? "Log in" : <SpinnerMini />}
         </Button>
-        <Button size="large" onClick={()=> navigate('/register')}>
-           Sign Up
-        </Button>
+        <div>
+          If you dont have account yet  
+          <br/>
+          <StyledLink to={navigate("/register")}> Sign Up</StyledLink>
+        </div>
+        
       </FormRowVertical>
     </form>
   );
